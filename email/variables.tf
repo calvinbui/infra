93b1859a81327a6453bdce83d8b677579b01cb92
Calvin Bui <3604363+calvinbui@users.noreply.github.com>
2025-07-12 22:48:02 +1000

    remove id

--- email/variables.tf
+++ email/variables.tf
@@ -1,3 +1,9 @@
+variable "account_id" {
+  type      = string
+  sensitive = false
+}
+
 variable "email_domain" {
+  type      = string
   sensitive = false
 }
--- email/zone.tf
+++ email/zone.tf
@@ -1,6 +1,6 @@
 resource "cloudflare_zone" "main" {
   account = {
-    id = "b4e8d4a2b400038b0c45bee1eb5c6219"
+    id = var.account_id
   }
 
   name = var.email_domain
