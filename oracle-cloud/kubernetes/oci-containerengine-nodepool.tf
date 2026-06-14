493e1332880196cbbe7677e4681ffde67d380514
Calvin Bui <3604363+calvinbui@users.noreply.github.com>
2026-06-14 22:21:36 +1000

    oke: change to new free tier limits

--- oracle-cloud/kubernetes/oci-containerengine-nodepool.tf
+++ oracle-cloud/kubernetes/oci-containerengine-nodepool.tf
@@ -19,7 +19,7 @@ resource "oci_containerengine_node_pool" "main" {
     node_pool_pod_network_option_details {
       cni_type          = oci_containerengine_cluster.main.cluster_pod_network_options[0].cni_type
       pod_subnet_ids    = [oci_core_subnet.pods.id]
-      max_pods_per_node = min((((4 / local.nodepool_size) == 1 ? 2 : (4 / local.nodepool_size)) - 1) * 31, 110)
+      max_pods_per_node = min((((2 / local.nodepool_size) == 1 ? 2 : (2 / local.nodepool_size)) - 1) * 31, 110)
     }
 
     is_pv_encryption_in_transit_enabled = true
@@ -28,8 +28,8 @@ resource "oci_containerengine_node_pool" "main" {
   node_shape = "VM.Standard.A1.Flex"
 
   node_shape_config {
-    ocpus         = 4 / local.nodepool_size
-    memory_in_gbs = 24 / local.nodepool_size
+    ocpus         = 2 / local.nodepool_size
+    memory_in_gbs = 12 / local.nodepool_size
   }
 
   node_source_details {
